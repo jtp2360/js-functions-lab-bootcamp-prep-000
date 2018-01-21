893da9f73ef3030a53e2096c11734e71f4dc5df3
Joao Neves <j_manuelneves@hotmail.com>
2018-01-21 15:54:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function happyHolidaysTo(name) {
   return "Happy holidays, you!"
 }
 function happyHolidayTo(holiday, name) {
-  return "Happy ${holiday}, ${name}!"
+  return `Happy ${holiday}, ${name}!`
 }
 function holidayCountdown(holiday, days) {
   return `It\'s 20 days until Mother\'s day!`
